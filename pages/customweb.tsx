d517b33f11398924638230bf802b9cbd01464e31
unknown <jayasingh.panorama@gmail.com>
2023-08-22 20:28:32 -0700

    Custom-SoftwareTest-Page

---
+++ pages/customweb.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import Image from 'next/image'
+import img6 from './custom.png'
+
+export default function customweb() {
+  return (
+    <>
+        <header className="h-80 w-100  bg-blue text-center">
+        
+        
+        
+        <div className="pt-16 ">
+            <h1 className="text-white text-6xl  font-bold ">CUSTOM WEB SOLUTION</h1>
+        </div>
+        
+        </header>
+        <div className='flex '>
+        <div>
+            <p className='leading-8  mx-20 mt-5  pt-20'>
+            Apart from standalone websites, we have also successfully established a variety
+             of web<br></br> portals, including  product and service listings for a variety of domains 
+             such as matrimonial<br></br>, education, employment, etc. Simply said, we have the skills
+              and capacity to design almost any <br></br>type of web portal you can think of - and we 
+              can do it quickly!
+            </p>
+
+            
+        </div>
+
+        
+        <div>
+
+        </div>
+        <Image src={img6} alt='logo' height={350} width={350}></Image>
+        </div>
+        <div>
+        <h1 className=' mx-16 pt-8 text-semibold text-3xl pl-2'>HOW WE WORK TO SOLVE YOUR PROBLEMS!</h1>
+        <div className='pt-8'>
+            <h1 className='text-4xl text-bold mx-16 my-4'>UNDERSTANDING</h1>
+            <p className='leading-8 mx-16 mt-5 pl-2'>It is important for us to understand present flaws and new requirements our clients
+             may have<br></br> while keeping the company type and it’s behaviour in mind in order to progress
+            forward.</p>
+
+        </div>
+        
+        <div className='pt-8'>
+            <h1 className='text-4xl text-bold mx-16 my-4 pl-2'>BRAINSTROMING</h1>
+            <p className='leading-8 mx-16 mt-5 pl-2'>Based on the knowledge, our team will assess the feasibility and challenges of 
+                every new<br></br> problem that we face.</p>
+        </div>
+        <div className='pt-8'>
+            <h1 className='text-4xl text-bold mx-16 my-4 pl-2'>SCOPE AND DESIGN</h1>
+            <p className='leading-8 mx-16 mt-5 pl-2'>The scope will be designed in such a way which will meet every client’s request
+                 while also<br></br> checking for future enhancements to provide the customer with a
+                  forecast on his requirements.</p>
+        </div>
+        <div className='pt-8'>
+            <h1 className='text-4xl text-bold mx-16 my-4 pl-2'>DEVELOPMENT</h1>
+            <p className='leading-8 mx-16 mt-5 pl-2' >We will make sure the product developed will meet your expectations 
+                and is delivered on time.</p>
+        </div>
+
+        <div className='pt-8 pb-10'>
+            <h1 className='text-4xl text-bold mx-16 my-4 pl-2'>TESTING AND DEPLOY</h1>
+            <p className='leading-8 mx-16 mt-5 pl-2'>Every solution undergoes a minimum of Three-Level testing to verify that it addresses all<br></br> aspects 
+                and we make sure that there are no errors involved.</p>
+        </div>
+        </div>
+        
+
+
+
+
+
+
+        </>
+  );
+}
---
+++ pages/softwaretest.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+// &rsquo;
+
+
+export default function softwaretest() {
+  return (
+    <>
+    <div className='pl-20 pt-20 pr-56'>
+    <h1 className='text-3xl font-semibold pb-10'>WHAT ARE THE GOALS OF SOFTWARE TESTING?</h1>
+<p className='pb-10'>Software testing ensures that a program is operating in accordance with its specifications. The QA team uses manual testing and 
+    automation testing to assess an application.<br></br></p>
+    <p className='pb-10'>Testing is a crucial aspect of the software development process. The application
+     will not be released into the public domain unless this is done.<br></br></p>
+     <p className='pb-10'>Both methods of testing ensure that a program is stable, engaging, and user-friendly.<br></br></p>
+     <p className='pb-10'>Testing, on the other hand, is a difficult task because every user has a different perspective and expectation. Also, 
+     we understand that not everyone will appreciate your product, but we should do everything 
+     we can to make it the best it can be.<br></br></p>
+     <p className='pb-10'>As a result, we make your application user-friendly through quality testing so that the 
+        largest number of people may enjoy it.<br></br></p>
+ </div>
+
+     
+ <div className='pl-20  pr-56'>
+    <h1 className='font-1xl font-semibold pb-5'>AIM OF SOFTWARE TESTING</h1>
+
+    <h1 className='font-1xl font-semibold pb-5'>Identification of Bugs and Errors
+    </h1>
+     <p className='pb-10'>The tester begins testing after the developer has finished
+      coding. QA validates each module under multiple scenarios throughout testing.<br></br></p>
+      <p className='pb-10'>They then gather all the mistakes and bugs and provide them to the developer to
+      be fixed.</p>
+ </div>
+
+    <div className='pl-20  pr-56'>
+    
+
+    <h1 className='font-1xl font-semibold pb-5'>Product of High Quality</h1>
+     <p className='pb-10'>The fundamental goal of testing is to keep the product’s quality high. Furthermore, 
+     testing has its own cycle, with each phase focusing solely on<br></br> quality.</p>
+    </div>
+
+    <div className='pl-20  pr-56'>
+    
+
+    <h1 className='font-1xl font-semibold pb-5'>Justification in the Form of a Requirement</h1>
+     <p className='pb-10'>The QA team verifies if the program adheres to the SRS (System Requirement Specification) document during testing.</p>
+    </div>
+
+    <div className='pl-20  pr-56'>
+    <h1 className='font-1xl font-semibold pb-5'>Provide Self-Assurance</h1>
+     <p className='pb-10'>The software’s features are continually checked by the testing team. 
+     It must meet the needs of the business while also instilling confidence.</p>
+    </div>
+
+    <div className='pl-20  pr-56'>
+    <h1 className='font-1xl font-semibold pb-5'>Enhance Buiseness Growth</h1>
+     <p className='pb-10'>A high-quality delivery boosts a company’s potential. It is integral to understand 
+     that we cannot compromise on quality, which can only be achieved through software testing.<br></br></p>
+
+    <p className='pb-10'>You must have seen the need for testing after reading
+     these objectives. Additionally, it will be a fantastic decision if you
+      want to pursue a job in this industry.</p>
+
+    </div>
+
+    <div className='pl-20  pr-56'>
+        <h1 className='font-1xl font-semibold pb-5'>CONCLUSION</h1>
+        <p className='pb-10'>After reading this article, we hope that you have some clarity regarding 
+        this vast topic. Software testing is an umbrella term consisting of numerous 
+        aspects. Some of its categories are:<p className='font-semibold'> Functional testing, Non-functional
+         testing, and regression and maintenance testing.</p> </p>
+
+         <p className='pb-10'>We hope you enjoyed our explanation of testing and deep analytical effort. 
+            Testing isn’t just a buzzword; it’s the lifeline of any internet product.</p>
+
+        <p className='pb-10'>We know it’s worth it as a testing organisation, and we always attempt to 
+            convey that to others. We support all types of testing methodologies,
+             including automation and manual testing.</p>
+
+        <p className='pb-10'>Likewise, we also have experience testing many types of software, web applications, 
+            and mobile applications. Panorama is always ready to assist you. Keep in contact
+             to learn more about quality assurance.</p>
+
+             <h1 className='font-1xl font-semibold pb-5 pb-20'>BY – RIYA DATTA</h1>
+    </div>
+
+    
+
+
+    </>
+  );
+}
